6a71948faa9d072540242c011b1d4fa2301599d2
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-10-10 11:01:17 -0400

    Initial commit. Base project.

---
+++ Jonathan_Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jonathan_Final
+//
+//  Created by Jonathan Reátegui on 2024-10-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jonathan_Final/Jonathan_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jonathan_FinalApp.swift
+//  Jonathan_Final
+//
+//  Created by Jonathan Reátegui on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct Jonathan_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
